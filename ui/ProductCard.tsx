3210a8957336aa7874509ad85b0b742c7cb43af0
kmamun716 <kazi299499@gmail.com>
2025-11-10 22:38:50 +0600

    some pages added

--- ui/ProductCard.tsx
+++ ui/ProductCard.tsx
@@ -10,7 +10,7 @@ const ProductCard = ({product}: any) => {
   
       return (
           <div
-              onClick={() => { router.push('/product/' + product._id); scrollTo(0, 0) }}
+              onClick={() => { router.push('/products/' + product._id); scrollTo(0, 0) }}
               className="flex flex-col items-start gap-0.5 max-w-[200px] w-full cursor-pointer"
           >
               <div className="cursor-pointer group relative bg-gray-500/10 rounded-lg w-full h-52 flex items-center justify-center">
